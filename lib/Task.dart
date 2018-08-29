91d38f18a25d30d444e5ccbac32c57e6b2905659
Paradoxon <two4god@gmail.com>
2018-08-29 15:01:15 +0200

    implemented Task class

---
+++ lib/Task.dart
@@ -0,0 +1,9 @@
+import 'package:save_the_world_flutter_app/Ressource.dart';
+
+class Task {
+  final List<Ressource> cost;
+  final List<Ressource> award;
+  final List<Ressource> require;
+
+  const Task({this.coste, this.award,this.require});
+}
