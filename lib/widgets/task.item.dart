2349dab124c7f6a182248d4057ba3ed6d3f0d350
Paradoxon <two4god@gmail.com>
2018-09-01 15:17:24 +0200

    fixed TaskItem

--- lib/widgets/task.item.dart
+++ lib/widgets/task.item.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:save_the_world_flutter_app/RessourceTable.dart';
 import 'package:save_the_world_flutter_app/models/task.model.dart';
+import 'package:save_the_world_flutter_app/widgets/ressourcetable.item.dart';
 
 class TaskItem extends StatelessWidget {
   final Task task;
@@ -10,10 +10,10 @@ class TaskItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      leading: RessourceTable(task.award),
+      leading: RessourceTable(ressourceList: task.cost),
       title: Text(task.name),
       subtitle: Text(task.description),
-      trailing: RessourceTable(task.cost)
-    ),
+      trailing: RessourceTable(ressourceList: task.award),
+    );
   }
 }
