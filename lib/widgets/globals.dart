321289112f551b67149c0b7a604f87f4045ee6ed
Paradoxon <two4god@gmail.com>
2018-09-02 15:09:12 +0200

    Added a global dart file for all global vars and strings for now we only added credits

---
+++ lib/widgets/globals.dart
@@ -0,0 +1,5 @@
+final List<String> credit = <String>[
+ "Icon made by freepik from www.flaticon.com",
+ "Icon made by becris from www.flaticon.com",
+ "Icon made by fjstudio from www.flaticon.com"
+];
