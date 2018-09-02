d2d66258b9589dbb1feeffb01ae397b08690a30f
Paradoxon <two4god@gmail.com>
2018-09-02 18:23:36 +0200

    added registration of the GameElement to the Modifier

--- lib/models/modifier.model.dart
+++ lib/models/modifier.model.dart
@@ -2,7 +2,29 @@ import 'package:save_the_world_flutter_app/models/gameelement.model.dart';
 import 'package:save_the_world_flutter_app/models/gameitem.model.dart';
 
 class Modifier extends GameItem {
-  final GameElement workOnItem;
-  final GameElement elementToWorkWith;
-  const Modifier({this.workOnItem,this.elementToWorkWith});
+  GameElement workOnItem;
+  GameElement elementToWorkWith;
+
+  Modifier({this.workOnItem,this.elementToWorkWith});
+
+  modify(){}
+
+  addedToElement(GameElement workOn){
+    if (workOnItem != null) {
+      removedFromElement(workOnItem);
+    }
+    workOnItem=workOn;
+  }
+
+  int removedFromElement(GameElement workedOn){
+    if (workedOn!=workOnItem)
+      return -1;
+    else{
+      //make shure it is removed from the gameElement
+      if (workedOn.hasModifyer(this)>=0)
+        workedOn.removeModifyer(this);
+      workOnItem=null;
+      return 0;
+    }
+  }
 }
