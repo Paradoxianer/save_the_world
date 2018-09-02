8ed035bf49249b6600763f12eec6f549ead43e00
Paradoxon <two4god@gmail.com>
2018-09-02 18:22:17 +0200

    add some more example TaskItems to check Scrolling

--- lib/models/ressource.model.dart
+++ lib/models/ressource.model.dart
@@ -3,9 +3,9 @@ import 'package:save_the_world_flutter_app/models/gameelement.model.dart';
 import 'package:save_the_world_flutter_app/models/modifier.model.dart';
 
 class Ressource extends GameElement {
-  final double min = 0.0;
-  final double value;
-  final double max = 100.0;
-  const Ressource ({String name,String description, IconData icon,this.value,List<Modifier> modifier}) :
+  double min = 0.0;
+  double value;
+  double max = 100.0;
+  Ressource ({String name,String description, IconData icon,this.value,List<Modifier> modifier}) :
     super(name: name,description : description, icon : icon);
 }
