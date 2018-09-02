b8cd8c561e509627a78f90c628a83c35f5300499
Paradoxon <two4god@gmail.com>
2018-09-02 18:20:37 +0200

    implemented Modifier handling

--- lib/models/gameelement.model.dart
+++ lib/models/gameelement.model.dart
@@ -3,6 +3,24 @@ import 'package:save_the_world_flutter_app/models/gameitem.model.dart';
 import 'package:save_the_world_flutter_app/models/modifier.model.dart';
 
 class GameElement extends GameItem {
-  final List<Modifier> myModifier;
-  const GameElement({String name, String description, IconData icon,this.myModifier}):super(name : name, description : description , icon : icon);
+  List<Modifier> myModifier;
+
+  GameElement({String name, String description, IconData icon,this.myModifier}):
+        super(name : name, description : description , icon : icon);
+
+  addModifyer(Modifier modifyThisElement){
+    myModifier.add(modifyThisElement);
+    modifyThisElement.addedToElement(this);
+  }
+
+  removeModifyer(Modifier modifierToRemove){
+    myModifier.remove(modifierToRemove);
+    modifierToRemove.removedFromElement(this);
+  }
+
+  int hasModifyer(Modifier mod){
+    return myModifier.indexOf(mod);
+  }
+
+
 }
