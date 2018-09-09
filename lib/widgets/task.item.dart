c7590c470d6bba27be5175ba31680d7a7309fd2d
Paradoxon <two4god@gmail.com>
2018-09-09 18:26:50 +0200

    implemented inital handleTap Method to react on a listitem tap

--- lib/widgets/task.item.dart
+++ lib/widgets/task.item.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:save_the_world_flutter_app/models/game.ressource.model.dart';
 import 'package:save_the_world_flutter_app/models/task.model.dart';
 import 'package:save_the_world_flutter_app/widgets/ressourcetable.item.dart';
+
 
 class TaskItem extends StatelessWidget {
   final Task task;
@@ -14,6 +16,15 @@ class TaskItem extends StatelessWidget {
       title: Text(task.name),
       subtitle: Text(task.description),
       trailing: RessourceTable(ressourceList: task.award),
+      onTap: _handleTap,
     );
+  }
+  void _handleTap() {
+    int listSize = task.cost.length;
+    for (int i = 0; i < listSize; i++) {
+      Game.ressources[task.cost[i].name].subtract(task.cost[i]);
+      }
+    task.modify();
+    Game.tasks.remove(this);
   }
 }
