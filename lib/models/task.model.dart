361381a0799f872da706ceea14daf2f71f52f5c3
Paradoxon <two4god@gmail.com>
2018-09-09 17:53:46 +0200

    added time to solve duration and startedAt to the Tasks model

--- lib/models/task.model.dart
+++ lib/models/task.model.dart
@@ -3,11 +3,22 @@ import 'package:save_the_world_flutter_app/models/modifier.model.dart';
 import 'package:save_the_world_flutter_app/models/ressource.model.dart';
 
 class Task extends GameElement {
+  DateTime duration;
+  DateTime timeToSolve;
+  DateTime startedAt;
+
   List<Ressource> cost;
   List<Ressource> award;
   List<Ressource> require;
 
   Task({String name,String description,this.cost, this.award,this.require, List<Modifier> modifer}) :
-        super(name : name, description : description, myModifier : modifer);
+        super(name : name, description : description, myModifier : modifer){
+    if (myModifier==null){
+      myModifier = new List<Modifier>();
+     // myModifier.add(Adder(award));
+      // myModifier.add(Subtractor(cost));
+
+    }
+  }
 
 }
