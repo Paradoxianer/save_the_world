4c157e37c83137e2313819612aa9cdaf078fe8f6
Paradoxon <two4god@gmail.com>
2018-09-09 20:02:59 +0200

    Converting RessourceItem to StatefullWidget --> this should lead to a updated Numbers when modified.. but we are not yet there

--- lib/widgets/ressource.item.dart
+++ lib/widgets/ressource.item.dart
@@ -1,11 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:save_the_world_flutter_app/models/ressource.model.dart';
 
-class RessourceItem extends StatelessWidget {
+class RessourceItem extends StatefulWidget {
   final Ressource ressource;
   double size;
 
   RessourceItem(this.ressource,[this.size=30.0]);
+  @override
+  RessourceItemState createState() => RessourceItemState(ressource, size);
+}
+
+
+class RessourceItemState extends State<RessourceItem> {
+  Ressource ressource;
+  double size;
+
+  RessourceItemState(this.ressource,[this.size=30.0]);
+
+
 
   @override
   Widget build(BuildContext context) {
