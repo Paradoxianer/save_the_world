f2fa3c5e1dd2345ced92e805c85f6094a96815ca
Paradoxon <two4god@gmail.com>
2018-09-09 18:26:10 +0200

    Added substract, add, canSubstract and canAdd Method to ressource so that it can be manipulated

--- lib/models/ressource.model.dart
+++ lib/models/ressource.model.dart
@@ -8,4 +8,40 @@ class Ressource extends GameElement {
   double max = 100.0;
   Ressource ({String name,String description, IconData icon,this.value,List<Modifier> modifier}) :
     super(name: name,description : description, icon : icon);
+
+  subtract(Ressource other){
+    this.value-=other.value;
+    if (this.value<min){
+      this.value=min;
+    }
+  }
+
+  add(Ressource other){
+    this.value+=other.value;
+    if (this.value>max){
+      this.value=max;
+    }
+  }
+
+  bool canAdd(Ressource other){
+    if (this.name == other.name){
+      if ((this.value+other.value)<max)
+        return true;
+      else
+        return false;
+    }
+    else
+      return false;
+  }
+
+  bool canSubtract(Ressource other){
+    if (this.name == other.name){
+      if ((this.value-other.value)>min)
+        return true;
+      else
+        return false;
+    }
+    else
+      return false;
+  }
 }
