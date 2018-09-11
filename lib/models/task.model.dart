5fb59874baccbd022a9fc5d5028eaeac6f5a2360
Paradoxon <two4god@gmail.com>
2018-09-11 20:06:21 +0200

    removed startedAt since we now use AnimationControler for monitoring the actual value the controler must be initalized fromt the State of the statefull Widget Also made it possible to pass duration and timeToSolve in the Constructor of a Task object Added start finished and missed methods which will calculate the given award and cost values. listen will listen for AnimationController events (maybe improove performance when we only listen to fnished events)

--- lib/models/task.model.dart
+++ lib/models/task.model.dart
@@ -1,24 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:save_the_world_flutter_app/models/game.ressource.model.dart';
 import 'package:save_the_world_flutter_app/models/gameelement.model.dart';
 import 'package:save_the_world_flutter_app/models/modifier.model.dart';
 import 'package:save_the_world_flutter_app/models/ressource.model.dart';
 
 class Task extends GameElement {
-  DateTime duration;
-  DateTime timeToSolve;
-  DateTime startedAt;
+  double duration;
+  double timeToSolve = double.infinity;
 
   List<Ressource> cost;
   List<Ressource> award;
   List<Ressource> require;
+  List<Task> missed;
 
-  Task({String name,String description,this.cost, this.award,this.require, List<Modifier> modifer}) :
+  AnimationController controller;
+
+  Task(
+      {String name, String description, this.cost, this.award, this.require, this.duration = 5000.0, this.timeToSolve = double
+          .infinity, List<Modifier> modifer, List<Modifier>missed = null}) :
         super(name : name, description : description, myModifier : modifer){
     if (myModifier==null){
       myModifier = new List<Modifier>();
-     // myModifier.add(Adder(award));
-      // myModifier.add(Subtractor(cost));
-
     }
   }
 
+  void miss() {
+    print("ohhh was ganz schlimmes ist passiert!!!");
+    if (missed != null) {
+      int listSize = missed.length;
+      for (int i = 0; i < listSize; i++) {
+        missed[i].modify();
+      }
+    }
+  }
+
+  start() {
+    if (controller.status != AnimationStatus.forward) {
+      controller.reset();
+      if (cost != null) {
+        int listSize = cost.length;
+        for (int i = 0; i < listSize; i++) {
+          Game.ressources[cost[i].name].subtract(cost[i]);
+        }
+      }
+      controller.forward();
+    }
+  }
+
+  finished() {
+    if (award != null) {
+      int listSize = award.length;
+      for (int i = 0; i < listSize; i++) {
+        Game.ressources[award[i].name].add(award[i]);
+      }
+    }
+    modify();
+    controller.reset();
+  }
+
+  listen() {
+    switch (controller.status) {
+      case AnimationStatus.dismissed:
+      //we need to check if its from reset or from reverse
+      //miss();
+        break;
+      case AnimationStatus.forward:
+        break;
+      case AnimationStatus.completed:
+        finished();
+        break;
+      case AnimationStatus.reverse:
+        break;
+    }
+  }
 }
