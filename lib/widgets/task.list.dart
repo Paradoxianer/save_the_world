2225cc27f22e56321124c405a813fd789e6f8472
Paradoxon <two4god@gmail.com>
2018-09-12 23:52:13 +0200

    Implemented a Statefull TaskList so that we can dynamically add and remove TaskListItems to the List

---
+++ lib/widgets/task.list.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:save_the_world_flutter_app/models/game.ressource.model.dart';
+import 'package:save_the_world_flutter_app/widgets/task.item.dart';
+
+class TaskList extends StatefulWidget {
+  @override
+  TaskListState createState() => TaskListState();
+}
+
+class TaskListState extends State<TaskList> {
+  TaskListState();
+
+  @override
+  void initState() {
+    super.initState();
+    print("addListener valueCahnged");
+    Game.getInstance().addListener(valueChanged);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    Game.getInstance().removeListener(valueChanged());
+  }
+
+  valueChanged() {
+    print("TaksList.valueChanged");
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemBuilder: (BuildContext context, int index) =>
+          TaskItem(task: Game.tasks[index]),
+      itemCount: Game.tasks.length,
+    );
+  }
+}
