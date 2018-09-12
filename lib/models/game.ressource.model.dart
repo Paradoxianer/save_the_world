87e174aa1f5056bc4eb10bd1a53da11510c0e100
Paradoxon <two4god@gmail.com>
2018-09-12 23:51:06 +0200

    moved the testList out of the game Model class into the gloabal file

--- lib/models/game.ressource.model.dart
+++ lib/models/game.ressource.model.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:save_the_world_flutter_app/models/faith.ressource.model.dart';
 import 'package:save_the_world_flutter_app/models/member.ressource.model.dart';
@@ -7,130 +8,53 @@ import 'package:save_the_world_flutter_app/models/ressource.model.dart';
 import 'package:save_the_world_flutter_app/models/task.model.dart';
 import 'package:save_the_world_flutter_app/models/time.ressource.model.dart';
 import 'package:save_the_world_flutter_app/models/wisdome.ressource.model.dart';
+import 'package:save_the_world_flutter_app/widgets/globals.dart';
 
-class Game{
+class Game {
+
   static Map<String,Ressource> ressources = new Map<String,Ressource>();
-
   static List<Task> tasks;
-
-  static TestVSync tick = new TestVSync();
+  static TestVSync tick;
+  static ChangeNotifier notifier;
+  static Game mInstance;
 
   Game(){
-    ressources[Faith().name]=Faith(value: 100.0);
+    notifier = new ChangeNotifier();
+    tick = new TestVSync();
+    ressources[Faith().name] = Faith(value: 250.0);
     ressources[Money().name]=Money(value: 10.0);
     ressources[Time().name]=Time(value: 24.0);
-    ressources[Member().name]=Member(value:1.0);
+    ressources[Member().name] = Member(value: 10.0);
     ressources[Publicity().name]=Publicity(value: 1.0);
     ressources[Wisdome().name]=Wisdome(value: 10.0);
-    tasks = <Task>[
-      Task(
-        name: "studieren",
-        description: "man lernt was",
-        cost:<Ressource>[
-          Money(value:200.0),
-          Time(value:1.5),
-        ],
-        award: <Ressource> [
-          Faith(value: 2.00),
-          Wisdome(value: 2.0),
-        ],
-      ),
-      Task(
-        name: "Wirtschaftsmission",
-        description: "durch die Kneipen ziehen und Geld sammeln",
-        cost:<Ressource>[
-          Money(value:20.0),
-          Time(value:4.0),
-        ],
-        award: <Ressource> [
-          Money(value: 100.00),
-          Publicity(value: 2.0),
-        ],
-      ),
-      Task(
-        name: "Beten für andere",
-        description: "wenn du für andere betest, dann passiert was",
-        cost:<Ressource>[
-          Time(value:1.0),
-        ],
-        award: <Ressource> [
-          Faith(value: 1.0),
-        ],
-      ),
-      Task(
-          name: "Predigt schreiben",
-          description: "wenn du eine Predigt geschrieben hast... kannst du auch eine halten :-D",
-          cost: <Ressource>[
-            Time(value: 8.0),
-            Faith(value: 100.0)
-          ],
-          award: <Ressource>[
-            Member(value: 0.02),
-          ],
-          duration: 2000.0
-      ),
-      Task(
-          name: "Kasse führen",
-          description: "die Kasse die muss in Ordnung sein",
-          cost: <Ressource>[
-            Time(value: 2.0)
-          ],
-          award: <Ressource>[
-            Money(value: 1.0),
-          ],
-          duration: 2000.0
-      ),
-      Task(
-          name: "Korps aufräumen",
-          description: "immer schön Ordnung schaffen. Wenn nicht gibt ein Problem mit den Mitgliedern :)",
-          cost:<Ressource>[
-            Time(value:1.0)
-          ],
-          award: null,
-          timeToSolve: 20000.0
-      ),
-      Task(
-        name: "Seelsorge",
-        description: "Pastor... ich hab da ein Problem",
-        cost:<Ressource>[
-          Time(value:1.0),
-          Wisdome(value: 1.0)
-        ],
-        award: <Ressource> [
-          Wisdome(value: 1.5)
-        ],
-      ),
-      Task(
-        name: "Bibellesen",
-        description: "Zeit mit Gott",
-        cost: <Ressource>[
-          Time(value: 1.0),
-        ],
-        award: <Ressource>[
-          Faith(value: 2.0),
-          Wisdome(value: 1.5)
-        ],
-      ),
-      Task(
-          name: "Mails...",
-          description: "Sie haben Post",
-          cost:<Ressource>[
-            Time(value:1.0)
-          ],
-          award: null,
-          timeToSolve: 50000.0
-      ),
-      Task(
-          name: "schlafen",
-          description: "Sollte man auch mal tun... das Gehirn benötig 8 Stunden Schlaf um \"schlauer zu werden\"",
-          cost: <Ressource>[
-            Time(value: 8.0)
-          ],
-          award: <Ressource>[
-            Time(value: 16.0)
-          ],
-          duration: 8000.0
-      ),
-    ];
+    tasks = testTasks;
+  }
+
+  void addTask(Task task) {
+    tasks.add(task);
+    notifier.notifyListeners();
+  }
+
+  void removeTask(Task task) {
+    tasks.remove(task);
+    print("removed: " + task.toString());
+    print("infomring" + notifier.toString());
+    notifier.notifyListeners();
+  }
+
+  static Game getInstance() {
+    if (mInstance == null) {
+      mInstance = new Game();
+    }
+    // Return MySingleton new Instance
+    return mInstance;
+  }
+
+  addListener(VoidCallback listener) {
+    notifier.addListener(listener);
+  }
+
+  removeListener(VoidCallback listener) {
+    notifier.removeListener(listener);
   }
 }
--- lib/widgets/globals.dart
+++ lib/widgets/globals.dart
@@ -1,5 +1,160 @@
+import 'package:save_the_world_flutter_app/models/faith.ressource.model.dart';
+import 'package:save_the_world_flutter_app/models/member.ressource.model.dart';
+import 'package:save_the_world_flutter_app/models/money.ressource.model.dart';
+import 'package:save_the_world_flutter_app/models/publicity.ressource.model.dart';
+import 'package:save_the_world_flutter_app/models/ressource.model.dart';
+import 'package:save_the_world_flutter_app/models/task.model.dart';
+import 'package:save_the_world_flutter_app/models/time.ressource.model.dart';
+import 'package:save_the_world_flutter_app/models/wisdome.ressource.model.dart';
+
+
 final List<String> credit = <String>[
- "Icon made by freepik from www.flaticon.com",
- "Icon made by becris from www.flaticon.com",
- "Icon made by fjstudio from www.flaticon.com"
+  "Icon made by freepik from www.flaticon.com",
+  "Icon made by becris from www.flaticon.com",
+  "Icon made by fjstudio from www.flaticon.com"
+];
+
+final List<Task> testTasks = <Task>[
+  Task(
+    name: "studieren",
+    description: "man lernt was",
+    cost: <Ressource>[
+      Money(value: 200.0),
+      Time(value: 8.0),
+    ],
+    award: <Ressource>[
+      Faith(value: 50.00),
+      Wisdome(value: 50.0),
+    ],
+  ),
+  Task(
+    name: "Wirtschaftsmission",
+    description: "durch die Kneipen ziehen und Geld sammeln",
+    cost: <Ressource>[
+      Money(value: 20.0),
+      Time(value: 4.5),
+    ],
+    award: <Ressource>[
+      Money(value: 100.00),
+      Publicity(value: 1.0),
+    ],
+  ),
+  Task(
+    name: "Beten für andere",
+    description: "wenn du für andere betest, dann passiert was",
+    cost: <Ressource>[
+      Time(value: 1.0),
+    ],
+    award: <Ressource>[
+      Faith(value: 1.0),
+      Member(value: 0.10),
+    ],
+  ),
+  Task(
+      name: "Predigt schreiben",
+      description: "wenn du eine Predigt geschrieben hast... kannst du auch eine halten :-D",
+      cost: <Ressource>[
+        Time(value: 8.0),
+        Faith(value: 100.0)
+      ],
+      award: <Ressource>[
+        Member(value: 0.02),
+        Faith(value: 20.0)
+      ],
+      duration: 8000.0
+  ),
+  Task(
+      name: "Kasse führen",
+      description: "die Kasse die muss in Ordnung sein",
+      cost: <Ressource>[
+        Time(value: 2.0)
+      ],
+      award: <Ressource>[
+        Money(value: 0.10),
+      ],
+      duration: 4000.0
+  ),
+  Task(
+      name: "Korps aufräumen",
+      description: "immer schön Ordnung schaffen. Wenn nicht gibt ein Problem mit den Mitgliedern :)",
+      cost: <Ressource>[
+        Time(value: 1.0)
+      ],
+      award: null,
+      timeToSolve: 20000.0,
+      duration: 2000.0
+  ),
+  Task(
+    name: "Seelsorge",
+    description: "Pastor... ich hab da ein Problem",
+    cost: <Ressource>[
+      Time(value: 1.0),
+      Wisdome(value: 1.0)
+    ],
+    award: <Ressource>[
+      Wisdome(value: 2.0),
+      Member(value: 0.25)
+    ],
+  ),
+  Task(
+      name: "Bibellesen",
+      description: "Zeit mit Gott",
+      cost: <Ressource>[
+        Time(value: 1.0),
+      ],
+      award: <Ressource>[
+        Faith(value: 2.0),
+        Wisdome(value: 1.5)
+      ],
+      duration: 2000.0
+  ),
+  Task(
+      name: "Mails...",
+      description: "Sie haben Post",
+      cost: <Ressource>[
+        Time(value: 1.0)
+      ],
+      award: null,
+      timeToSolve: 50000.0,
+      duration: 2000.0
+  ),
+  Task(
+      name: "schlafen",
+      description: "Sollte man auch mal tun... das Gehirn benötig 8 Stunden Schlaf um \"schlauer zu werden\"",
+      cost: <Ressource>[
+        Time(value: 8.0)
+      ],
+      award: <Ressource>[
+        Time(value: 16.0)
+      ],
+      duration: 16000.0
+  ),
+  Task(
+      name: "Gottesdienst halten",
+      description: "schön mit Predigt, lieder und natürlich Bibelles",
+      cost: <Ressource>[
+        Member(value: 2.0),
+        Time(value: 2.0),
+        Faith(value: 200.0)
+      ],
+      award: <Ressource>[
+        Faith(value: 250.0),
+        Member(value: 2.5),
+        Money(value: 20.0)
+      ],
+      duration: 4000.0
+  ),
+  Task(
+      name: "Spender anschreiben",
+      description: "vielleicht bringt ja ein netter Brief was",
+      cost: <Ressource>[
+        Time(value: 4.0),
+        Faith(value: 50.0)
+      ],
+      award: <Ressource>[
+        Faith(value: 40.0),
+        Money(value: 10.0)
+      ],
+      duration: 8000.0
+  ),
 ];
