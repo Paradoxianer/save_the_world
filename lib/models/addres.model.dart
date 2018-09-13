7d63e875614c8e2a2883c99448e0d1b2714a618b
Paradoxon <two4god@gmail.com>
2018-09-13 21:35:12 +0200

    Made this Modifier more flexible

--- lib/models/addres.model.dart
+++ lib/models/addres.model.dart
@@ -1,3 +1,4 @@
+import 'package:save_the_world_flutter_app/models/game.ressource.model.dart';
 import 'package:save_the_world_flutter_app/models/gameelement.model.dart';
 import 'package:save_the_world_flutter_app/models/modifier.model.dart';
 import 'package:save_the_world_flutter_app/models/ressource.model.dart';
@@ -7,12 +8,24 @@ class AddRes extends Modifier {
   List<Ressource> ressources;
   Map<String, Ressource> workOnRes;
 
-  AddRes({this.ressources, this.workOnRes}) : super();
+  AddRes(List<Ressource> ressources, [Map<String, Ressource> workOnRes = null])
+      :
+        super(name: "AddRes", description: "Adds a list of Ressource") {
+    this.ressources = ressources;
+    this.workOnRes = workOnRes;
+  }
 
   modify() {
     int listSize = ressources.length;
-    for (int i = 0; i < listSize; i++) {
-      workOnRes[ressources[i].name].add(ressources[i]);
+    if (workOnRes != null) {
+      for (int i = 0; i < listSize; i++) {
+        workOnRes[ressources[i].name].add(ressources[i]);
+      }
+    }
+    else {
+      for (int i = 0; i < listSize; i++) {
+        Game.ressources[ressources[i].name].add(ressources[i]);
+      }
     }
   }
 }
