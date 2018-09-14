f1e239436cf66d24534184d7e907bc738d604543
Paradoxon <two4god@gmail.com>
2018-09-14 07:35:19 +0200

    Remove Modifier Modifier implementation

---
+++ lib/models/removemodifier.model.dart
@@ -0,0 +1,31 @@
+import 'package:save_the_world_flutter_app/models/game.ressource.model.dart';
+import 'package:save_the_world_flutter_app/models/gameelement.model.dart';
+import 'package:save_the_world_flutter_app/models/modifier.model.dart';
+import 'package:save_the_world_flutter_app/models/task.model.dart';
+
+class RemoveModifer extends Modifier {
+  GameElement workOnItem;
+  String nameOfTask;
+  List<Modifier> mymodifer;
+
+  RemoveModifer({String task, List<Modifier> modifier})
+      : super(
+            name: "AddModifer",
+            description: "Adds the given list of Modifier to the given task") {
+    this.nameOfTask = task;
+    this.mymodifer = modifier;
+  }
+
+  modify() {
+    Task found;
+    if (nameOfTask != null)
+      found = Game.tasks.firstWhere((tsk) => tsk.name == nameOfTask);
+    else
+      found = workOnItem;
+    if (found != null) {
+      int i = 0;
+      for (i = 0; i < mymodifer.length; i++)
+        found.myModifier.remove(mymodifer[i]);
+    }
+  }
+}
