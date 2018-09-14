b7e1351f66b4225d2b7600cea68fb9118fc1c28e
Paradoxon <two4god@gmail.com>
2018-09-14 07:21:10 +0200

    Implemented Modier AddModifier which will add a List of Modifer to the given Task

---
+++ lib/models/addmodifier.model.dart
@@ -0,0 +1,25 @@
+import 'package:save_the_world_flutter_app/models/game.ressource.model.dart';
+import 'package:save_the_world_flutter_app/models/gameelement.model.dart';
+import 'package:save_the_world_flutter_app/models/modifier.model.dart';
+import 'package:save_the_world_flutter_app/models/task.model.dart';
+
+class AddModifer extends Modifier {
+  GameElement workOnItem;
+  String nameOfTask;
+  List<Modifier> mymodifer;
+
+  AddModifer({String task, List<Modifier> modifier})
+      : super(
+            name: "AddModifer",
+            description: "Adds the given list of Modifier to the given task") {
+    this.nameOfTask = task;
+    this.mymodifer = modifier;
+  }
+
+  modify() {
+    Task found = Game.tasks.firstWhere((tsk) => tsk.name == nameOfTask);
+    if (found != null) {
+      found.myModifier.addAll(mymodifer);
+    }
+  }
+}
