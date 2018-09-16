4ba8f785843c4c2701222e36111650c616fe90a7
Paradoxon <two4god@gmail.com>
2018-09-16 21:05:13 +0200

    fixed the implementation for SubtractRes

--- lib/models/subtractres.model.dart
+++ lib/models/subtractres.model.dart
@@ -1,3 +1,4 @@
+import 'package:save_the_world_flutter_app/models/game.ressource.model.dart';
 import 'package:save_the_world_flutter_app/models/gameelement.model.dart';
 import 'package:save_the_world_flutter_app/models/modifier.model.dart';
 import 'package:save_the_world_flutter_app/models/ressource.model.dart';
@@ -7,12 +8,19 @@ class SubtractRes extends Modifier {
   List<Ressource> ressources;
   Map<String, Ressource> workOnRes;
 
-  SubtractRes({this.ressources, this.workOnRes}) : super();
+  SubtractRes({this.ressources, this.workOnRes = null}) : super();
 
   modify() {
     int listSize = ressources.length;
-    for (int i = 0; i < listSize; i++) {
-      workOnRes[ressources[i].name].subtract(ressources[i]);
+    if (workOnRes != null) {
+      for (int i = 0; i < listSize; i++) {
+        workOnRes[ressources[i].name].subtract(ressources[i]);
+      }
+    }
+    else {
+      for (int i = 0; i < listSize; i++) {
+        Game.ressources[ressources[i].name].subtract(ressources[i]);
+      }
     }
   }
 }
