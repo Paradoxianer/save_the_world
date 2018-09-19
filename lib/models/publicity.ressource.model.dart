ced5e43f54488fa1487a40e80e065d4078a868a9
Paradoxon <two4god@gmail.com>
2018-09-19 23:44:29 +0200

    Implemented Json for Publicity

--- lib/models/publicity.ressource.model.dart
+++ lib/models/publicity.ressource.model.dart
@@ -2,12 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:save_the_world_flutter_app/models/ressource.model.dart';
 
 class Publicity extends Ressource {
-  Publicity({double value}):
+  Publicity({double value}) :
         super(
-          name:"Bekanntheit",
-          description:"wie bekannt du bist",
-          icon : Icons.live_tv,
+          name: "Bekanntheit",
+          description: "wie bekannt du bist",
+          icon: Icons.live_tv,
           value: value,
           modifier: null
-  );
+      );
+
+  factory Publicity.fromJson(Map<String, dynamic> json){
+    return Publicity(value: json['value']);
+  }
+
+  Map<String, dynamic> toJson() {
+    return <String, dynamic>{
+      'name': name,
+      'value': value
+    };
+  }
 }
