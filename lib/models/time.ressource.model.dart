3bcb2bcd38d74aac315a134e23d45e202417c439
Paradoxon <two4god@gmail.com>
2018-09-19 23:45:41 +0200

    Implemented Json for Time

--- lib/models/time.ressource.model.dart
+++ lib/models/time.ressource.model.dart
@@ -2,16 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:save_the_world_flutter_app/models/ressource.model.dart';
 
 class Time extends Ressource {
-  Time({double value}):
+  Time({double value}) :
         super(
-          name:"Zeit",
-          description:"wieviel Zeit du hast",
-          icon : Icons.access_time,
+          name: "Zeit",
+          description: "wieviel Zeit du hast",
+          icon: Icons.access_time,
           value: value,
           modifier: null
-      )
-  {
+      ) {
     this.min = 0.0;
     this.max = 24.0;
+  }
+
+  factory Time.fromJson(Map<String, dynamic> json){
+    return Time(value: json['value']);
+  }
+
+  Map<String, dynamic> toJson() {
+    return <String, dynamic>{
+      'name': name,
+      'value': value
+    };
   }
 }
