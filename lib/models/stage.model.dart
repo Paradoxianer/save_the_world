c50c0cd16118425c63fadeada6c9ac303360c5f0
Paradoxon <two4god@gmail.com>
2018-10-07 19:11:38 +0200

    Added a new Stage Class this class will store all data which belongs to a certain stage... like the tasklist, description.... list of the active Tasks list of the random Tasks. and so on

---
+++ lib/models/stage.model.dart
@@ -0,0 +1,8 @@
+import 'package:save_the_world_flutter_app/models/task.model.dart';
+
+class Stage {
+  String description;
+  List<String> activeTasks;
+  List<String> randomTasks;
+  List<Task> allTasks;
+}
