6e220abfd478ed946cb286353bf06800346fe622
Paradoxon <two4god@gmail.com>
2018-10-23 08:18:51 +0200

    added number of members and levelnummer and a default constuctor

--- lib/models/stage.model.dart
+++ lib/models/stage.model.dart
@@ -1,8 +1,12 @@
 import 'package:save_the_world_flutter_app/models/task.model.dart';
 
 class Stage {
+  int level;
+  int member;
   String description;
   List<String> activeTasks;
   List<String> randomTasks;
   List<Task> allTasks;
+
+  Stage({this.level, this.activeTasks, this.randomTasks, this.allTasks});
 }
