7f5a493e9a460c9e818fe9e083c857759dc81502
Paradoxon <two4god@gmail.com>
2018-11-04 18:26:12 +0100

    renamed the stages list to levelist since stages now contains all the sub tasks and lists.

--- lib/widgets/level.list.dart
+++ lib/widgets/level.list.dart
@@ -4,7 +4,7 @@ import 'package:save_the_world_flutter_app/globals.dart';
 class LevelList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    List<int> stageList = stages.keys.toList();
+    List<int> stageList = levels.keys.toList();
     return ListView.builder(
       itemBuilder: (BuildContext context, int index) {
         return ListTile(
@@ -16,10 +16,10 @@ class LevelList extends StatelessWidget {
               )
           ),
           title: Text("bis " + stageList[index].toString() + " Mitglieder"),
-          subtitle: Text(stages[stageList[index]]),
+          subtitle: Text(levels[stageList[index]]),
         );
       },
-      itemCount: stages.length,
+      itemCount: levels.length,
     );
   }
 }
