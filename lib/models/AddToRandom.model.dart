875656bd5f4c811c01df04989d77936e69f07828
Paradoxon <two4god@gmail.com>
2018-11-15 20:46:51 +0100

    Add AddToRandom Modifier

---
+++ lib/models/AddToRandom.model.dart
@@ -0,0 +1,23 @@
+import 'package:save_the_world_flutter_app/models/game.ressource.model.dart';
+import 'package:save_the_world_flutter_app/models/modifier.model.dart';
+
+class AddToRandom extends Modifier {
+  String task;
+
+  AddToRandom({this.task})
+      : super(
+            name: "AddToRandom",
+            description: "Adds the given String to the Random List") {}
+
+  factory AddToRandom.fromJson(Map<String, dynamic> jsn) {
+    return AddToRandom(task: jsn['task']);
+  }
+
+  Map<String, dynamic> toJson() {
+    return <String, dynamic>{'name': name, 'task': task};
+  }
+
+  modify() {
+    Game.getInstance().randomTasks.add(task);
+  }
+}
