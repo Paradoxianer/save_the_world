8da20753944017852747421e5c5193a7dc404763
Paradoxon <two4god@gmail.com>
2019-05-05 18:52:58 +0200

    show the newStage dialog when the user reaches next level

--- lib/widgets/stage.item.dart
+++ lib/widgets/stage.item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:save_the_world_flutter_app/globals.dart';
 import 'package:save_the_world_flutter_app/models/game.ressource.model.dart';
 
 class StageItem extends StatefulWidget {
@@ -14,12 +15,15 @@ class StageItemState extends State<StageItem> {
   Game game;
   double size;
 
+
   StageItemState([this.size = 30.0]) {
     game = Game.getInstance();
     game.addStageListener(valueChanged);
   }
 
   valueChanged() {
+    print("show the dialog");
+    showNewStage(context);
     setState(() {});
   }
 
@@ -34,4 +38,46 @@ class StageItemState extends State<StageItem> {
       ],
     );
   }
+}
+
+void showNewStage(BuildContext context) {
+  showDialog(
+      context: context,
+      barrierDismissible: true, // user must tap button for close dialog!
+      builder: (BuildContext context) {
+        List<int> stageList = levels.keys.toList();
+        return SimpleDialog(
+            title: Text('Gratulation'),
+            children: <Widget>[
+              Stack(
+                  children: <Widget>[
+                    Align(
+                        alignment: Alignment.center,
+                        child: Image.asset('assets/icons/award.png')
+                    ),
+                    Positioned(
+                        left: 124.0,
+                        top: 36.0,
+                        child: Text(Game
+                            .getInstance()
+                            .stage
+                            .toString(),
+                            textScaleFactor: (4)
+                        )
+                    )
+                  ]
+              ),
+              Text("\n du bist jetzt eine", textAlign: TextAlign.center),
+              Text("" + levels[stageList[Game
+                  .getInstance()
+                  .stage]] + "\n", textAlign: TextAlign.center),
+              Text("du kannst jetzt bis " + stageList[Game
+                  .getInstance()
+                  .stage].toString() + " Mitglieder erreichen",
+                  textAlign: TextAlign.center),
+
+            ]
+        );
+      }
+  );
 }
