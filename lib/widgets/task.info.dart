adb17b6ff913db18df15e01839703b1f4c3667d8
Paradoxon <two4god@gmail.com>
2019-05-11 18:47:03 +0200

    improofed the look of taskinfo a lot

--- lib/widgets/task.info.dart
+++ lib/widgets/task.info.dart
@@ -7,36 +7,56 @@ void showTaskInfo(BuildContext context, Task tsk) {
       context: context,
       barrierDismissible: true, // user must tap button for close dialog!
       builder: (BuildContext context) {
-        return SimpleDialog(title: Text(tsk.name), children: <Widget>[
-          Column(
+        return SimpleDialog(
+            title: Text(tsk.name),
+            contentPadding: EdgeInsets.all(6.0),
             children: <Widget>[
-              Text(tsk.description),
-              Table(
-                children: <TableRow>[
-                  TableRow(children: <Widget>[
-                    Text("Dauer:"),
-                    Text(tsk.duration.toString())
-                  ]),
-                  TableRow(children: <Widget>[
-                    Text("Zeit zum lösen:"),
-                    Text(tsk.timeToSolve.toString()),
-                  ])
-                ],
-              ),
-              Row(
+              Column(
                 children: <Widget>[
-                  Text("Kosten:"),
-                  RessourceTable(ressourceList: tsk.cost)
-                ],
-              ),
-              Row(
-                children: <Widget>[
-                  Text("Belohnung:"),
-                  RessourceTable(ressourceList: tsk.award)
+                  Text(tsk.description),
+                  SingleChildScrollView(
+                    child: Table(
+                      children: <TableRow>[
+                        TableRow(children: <Widget>[
+                          Text("Dauer:"),
+                          Text(tsk.duration.toString())
+                        ]),
+                        TableRow(children: <Widget>[
+                          Text("Zeit zum lösen:"),
+                          Text(tsk.timeToSolve.toString()),
+                        ]),
+                        TableRow(children: <Widget>[
+                          Text("Kosten:"),
+                          RessourceTable(ressourceList: tsk.cost)
+                        ]),
+                        TableRow(children: <Widget>[
+                          Text("Belohnung:"),
+                          RessourceTable(ressourceList: tsk.award)
+                        ]),
+                        TableRow(children: <Widget>[
+                          Text("Verpasst:"),
+                          Column(
+                              children: tsk.missed
+                                  .map(
+                                      (mm) => new Text(mm.name, softWrap: true))
+                                  .toList())
+                        ]),
+                        TableRow(children: <Widget>[
+                          Text("erfüllt:"),
+                          Column(
+                              children: tsk.myModifier
+                                  .map((mm) =>
+                              new Text(
+                                mm.name,
+                                softWrap: true,
+                              ))
+                                  .toList())
+                        ])
+                      ],
+                    ),
+                  )
                 ],
               )
-            ],
-          )
-        ]);
+            ]);
       });
 }
