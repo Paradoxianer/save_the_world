f2fecbe48057e40c8d0bd7c6fdba16b07b46512a
Matthias Lindner <two4god@gmail.com>
2026-03-14 20:10:20 +0100

    refactor: fix task name conversion in stage editor export by using name mapping

--- lib/editor/utils/stage_export_utils.dart
+++ lib/editor/utils/stage_export_utils.dart
@@ -13,14 +13,45 @@ import 'package:save_the_world_flutter_app/models/removemodifier.model.dart';
 import 'package:save_the_world_flutter_app/models/subtractres.model.dart';
 
 class StageExportUtils {
+  // Mapping von Anzeigenamen zu den ursprünglichen Variablennamen in common_tasks.dart
+  static const Map<String, String> _libraryNameMapping = {
+    "Schlafen": "baseSleep",
+    "Freizeit": "baseFreeTime",
+    "Bibellesen": "baseBible",
+    "Kollekte": "collectMoney",
+    "Der Heilige Geist möchte wirken": "holySpiritWorking",
+    "Heiratsvorbereitung 1": "weddingPhase1",
+    "Heiratsvorbereitung 2": "weddingPhase2",
+    "Hochzeit": "actualWedding",
+    "Jemand möchte heiraten": "someoneWantsToMarry",
+    "Beerdigung eines Generals": "funeralGeneral",
+  };
+
   static String exportLibrary(List<Task> libraryTasks) {
     final buffer = StringBuffer();
     buffer.writeln("import 'package:save_the_world_flutter_app/models/addtask.model.dart';");
-    // ... (rest of the imports)
+    buffer.writeln("import 'package:save_the_world_flutter_app/models/removetask.model.dart';");
+    buffer.writeln("import 'package:save_the_world_flutter_app/models/setmax.model.dart';");
+    buffer.writeln("import 'package:save_the_world_flutter_app/models/setmin.model.dart';");
+    buffer.writeln("import 'package:save_the_world_flutter_app/models/autoexecute.model.dart';");
+    buffer.writeln("import 'package:save_the_world_flutter_app/models/AddToRandom.model.dart';");
+    buffer.writeln("import 'package:save_the_world_flutter_app/models/message.modifier.dart';");
+    buffer.writeln("import 'package:save_the_world_flutter_app/models/removemodifier.model.dart';");
+    buffer.writeln("import 'package:save_the_world_flutter_app/models/subtractres.model.dart';");
+    buffer.writeln("import 'package:save_the_world_flutter_app/models/task.model.dart';");
+    buffer.writeln("import 'package:save_the_world_flutter_app/models/faith.ressource.model.dart';");
+    buffer.writeln("import 'package:save_the_world_flutter_app/models/member.ressource.model.dart';");
+    buffer.writeln("import 'package:save_the_world_flutter_app/models/money.ressource.model.dart';");
+    buffer.writeln("import 'package:save_the_world_flutter_app/models/publicity.ressource.model.dart';");
+    buffer.writeln("import 'package:save_the_world_flutter_app/models/time.ressource.model.dart';");
+    buffer.writeln("import 'package:save_the_world_flutter_app/models/wisdome.ressource.model.dart';");
+    buffer.writeln();
     buffer.writeln('// --- AUTO-GENERATED LIBRARY EXPORT ---');
     for (var t in libraryTasks) {
-      String varName = t.name.replaceAll(" ", "").replaceAll(RegExp(r'[^a-zA-Z0-9]'), '');
-      varName = varName.replaceAll('ä', 'ae').replaceAll('ö', 'oe').replaceAll('ü', 'ue').replaceAll('ß', 'ss');
+      // Prüfe erst im Mapping, sonst generiere einen Namen
+      String varName = _libraryNameMapping[t.name] ?? 
+                       t.name.replaceAll(" ", "").replaceAll(RegExp(r'[^a-zA-Z0-9]'), '')
+                             .replaceAll('ä', 'ae').replaceAll('ö', 'oe').replaceAll('ü', 'ue').replaceAll('ß', 'ss');
       
       buffer.writeln('final Task $varName = Task(');
       buffer.writeln('  name: "${t.name}",');
@@ -58,23 +89,28 @@ class StageExportUtils {
     buffer.writeln('  randomTasks: ${_formatStringList(randomTasks)},');
     buffer.writeln('  allTasks: [');
     for (var t in allStageTasks) {
-      buffer.writeln('    Task( ');
-      buffer.writeln('      name: "${t.name}",');
-      buffer.writeln('      description: "${t.description}",');
-      buffer.writeln('      duration: ${t.duration},');
-      if (t.timeToSolve != double.infinity) {
-        buffer.writeln('      timeToSolve: ${t.timeToSolve},');
+      // Prüfe ob der Task aus der Library kommt
+      if (_libraryNameMapping.containsKey(t.name)) {
+        buffer.writeln('    ${_libraryNameMapping[t.name]},');
+      } else {
+        buffer.writeln('    Task( ');
+        buffer.writeln('      name: "${t.name}",');
+        buffer.writeln('      description: "${t.description}",');
+        buffer.writeln('      duration: ${t.duration},');
+        if (t.timeToSolve != double.infinity) {
+          buffer.writeln('      timeToSolve: ${t.timeToSolve},');
+        }
+        buffer.writeln('      isMilestone: ${t.isMilestone},');
+        buffer.writeln('      cost: [${_exportResources(t.cost)}],');
+        buffer.writeln('      award: [${_exportResources(t.award)}],');
+        final modStr = _exportModifiers(t.myModifier ?? []);
+        if (modStr.isNotEmpty) buffer.writeln('      modifier: [$modStr], ');
+        final onStr = _exportModifiers(t.online ?? []);
+        if (onStr.isNotEmpty) buffer.writeln('      online: [$onStr], ');
+        final missStr = _exportModifiers(t.missed ?? []);
+        if (missStr.isNotEmpty) buffer.writeln('      missed: [$missStr], ');
+        buffer.writeln('    ),');
       }
-      buffer.writeln('      isMilestone: ${t.isMilestone},');
-      buffer.writeln('      cost: [${_exportResources(t.cost)}],');
-      buffer.writeln('      award: [${_exportResources(t.award)}],');
-      final modStr = _exportModifiers(t.myModifier ?? []);
-      if (modStr.isNotEmpty) buffer.writeln('      modifier: [$modStr], ');
-      final onStr = _exportModifiers(t.online ?? []);
-      if (onStr.isNotEmpty) buffer.writeln('      online: [$onStr], ');
-      final missStr = _exportModifiers(t.missed ?? []);
-      if (missStr.isNotEmpty) buffer.writeln('      missed: [$missStr], ');
-      buffer.writeln('    ),');
     }
     buffer.writeln('  ],');
     buffer.writeln(');');
